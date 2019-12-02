4da85299135489df8575a8f1e0574a2ec341159b
Onur Y. Özcan <ozcano@users.noreply.github.com>
2019-12-02 14:35:06 +0300

    variable stuff

--- test.py
+++ test.py
@@ -2,11 +2,7 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-print("Bye bye love")
-
-print("no conflict")
-
-print("new collaborator")
-
-print("Something New")
-print("1 satır daha")
+a = 2
+b = 5
+c = a * b
+print(c)
